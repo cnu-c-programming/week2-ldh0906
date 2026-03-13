9f57b2bac504188b04412b395f1e4332262a92aa
ldh0906 <a30308104@gmail.com>
2026-03-13 12:18:28 +0900

    assignment

--- code/4-1.c
+++ code/4-1.c
@@ -2,6 +2,23 @@
 
 int main()
 {
+    int a, b;
+    char operator;
+    scanf("%d %d %c", &a, &b, &operator);
+    
+    if(operator == '+') {
+        printf("%d", a+b);
+    } else if (operator == '-') {
+        printf("%d", a-b);
+    } else if (operator == '/') {
+        printf("%d", a/b);
+    } else if (operator == '*') {
+        printf("%d", a*b);
+    } else {
+        printf("error");
+    }
+    
+    
 
     return 0;
 }
--- code/4-3.c
+++ code/4-3.c
@@ -2,7 +2,17 @@
 
 int main()
 {
-
+    int input;
+    scanf("%d", &input);
+    
+    for (int i = 2; i < input; i++) {
+        if (input % i == 0) {
+            printf("false");
+            goto label;
+        } 
+    }
+    printf("true");
+    label:
     return 0;
 }
 
